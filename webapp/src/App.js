645d5a121d5c02faf42df17e45402a09db8aeab0
JP Saraceno <jp@scvsoft.com>
2020-12-11 16:12:30 -0300

    made cells editable

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { update } from "ramda"
 
 import useInterval from "hooks/useInterval"
 import Map from "components/Map"
@@ -30,9 +31,20 @@ function App() {
     running ? 1000 : null
   )
 
+  const onCellClick = (row, column) => {
+    if (!running) {
+      const newMap = update(
+        row,
+        update(column, map[row][column] === 0 ? 1 : 0, map[row]),
+        map
+      )
+      setMap(newMap)
+    }
+  }
+
   return (
     <div className="App">
-      <Map mapRows={map} />
+      <Map mapRows={map} onCellClick={onCellClick} editable={!running} />
       <Button
         label={running ? "Stop" : "Start"}
         onClick={() => {
--- webapp/src/components/Map/index.js
+++ webapp/src/components/Map/index.js
@@ -1,24 +1,41 @@
 import React from "react"
 import styles from "./styles.module.css"
+import { curry } from "ramda"
 
-const Cell = ({ value }) => (
+const Cell = ({ value, editable, onClick }) => (
   <div
-    className={`${styles.cell} ${value ? styles.liveCell : styles.deadCell}`}
+    className={`${styles.cell} 
+      ${value ? styles.liveCell : styles.deadCell}
+      ${editable ? styles.editable : styles.nonEditable}
+    `}
+    onClick={onClick}
   ></div>
 )
 
-const Row = ({ cells = [] }) => (
+const Row = ({ cells = [], editable = false, onCellClick }) => (
   <div className={styles.row}>
     {cells.map((cellValue, index) => (
-      <Cell value={cellValue} key={`cell_${index}`} />
+      <Cell
+        editable={editable}
+        value={cellValue}
+        key={`cell_${index}`}
+        onClick={() => {
+          onCellClick(index)
+        }}
+      />
     ))}
   </div>
 )
 
-const Map = ({ mapRows = [] }) => (
+const Map = ({ mapRows = [], editable = false, onCellClick = () => {} }) => (
   <div className={styles.map}>
     {mapRows.map((rowCells, index) => (
-      <Row cells={rowCells} key={`row_${index}`} />
+      <Row
+        editable={editable}
+        cells={rowCells}
+        key={`row_${index}`}
+        onCellClick={curry(onCellClick)(index)}
+      />
     ))}
   </div>
 )
